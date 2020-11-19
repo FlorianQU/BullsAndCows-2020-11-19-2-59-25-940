ac136a4f29b3228c9fdb56b8a4dbe07a23d9a00c
hockeyqqy <qqu@slb.com>
2020-11-19 15:54:18 +0800

    feat: pass test with testsecretgenerator

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,16 +5,23 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private readonly string secret = string.Empty;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            this.secret = secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            return this.Compare(this.secret, guess);
+        }
+
+        private string Compare(string secret, string guess)
+        {
+            return secret == guess ? "4A0B" : string.Empty;
         }
     }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -8,10 +8,31 @@ namespace BullsAndCowsTest
         [Fact]
         public void Should_create_BullsAndCowsGame()
         {
-            var secretGenerator = new SecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
+            var testSecretGenerator = new TestSecretGenerator();
+            //var secretGenerator = new SecretGenerator();
+            var game = new BullsAndCowsGame(testSecretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void Should_Return_0A0B_when_all_digit_and_position_wrong()
+        {
+            //given
+            var testSecretGenerator = new TestSecretGenerator();
+            var game = new BullsAndCowsGame(testSecretGenerator);
+            //when
+            string answer = game.Guess("1234");
+            //then
+            Assert.Equal("4A0B", answer);
+        }
+
+        public class TestSecretGenerator : SecretGenerator
+        {
+            public override string GenerateSecret()
+            {
+                return "1234";
+            }
         }
     }
 }
